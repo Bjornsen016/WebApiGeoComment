0bd5ad5d68b6e318433f81cbae4b89922855a24e
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-04-19 11:37:17 +0200

    Started building a controller for the GeoCommentAPI

---
+++ GeoComment/Controllers/GeoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GeoComment.Controllers;
+
+[Route("api/geo-comments")]
+[ApiController]
+public class GeoController : ControllerBase
+{
+    [Route("{id}")]
+    [HttpGet]
+    public ActionResult<Comment> Get(int id, [BindRequired] [FromQuery(Name = "api-version")] string apiVersion)
+    {
+        var comment = new Comment
+        {
+            Id = 1,
+            Author = "Ada",
+            Latitude = 10,
+            Longitude = 5,
+            Message = "Lorem ipsum dolor amet"
+        };
+        return Ok(comment);
+    }
+
+    [HttpGet]
+    public IEnumerable<string> GetList(double minLon, double maxLon, double minLat, double maxLat,
+        [BindRequired] [FromQuery(Name = "api-version")]
+        string apiVersion)
+    {
+        throw new NotImplementedException();
+    }
+}
--- GeoComment/Program.cs
+++ GeoComment/Program.cs
@@ -1,3 +1,5 @@
+global using GeoComment.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
