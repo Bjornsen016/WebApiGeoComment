75f441bba3a8cfbc79f7737986e92256125413db
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-04-22 09:25:42 +0200

    Adjusted Program.cs for dbctx

--- GeoComment/Program.cs
+++ GeoComment/Program.cs
@@ -1,4 +1,6 @@
 global using GeoComment.Models;
+using GeoComment.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<GeoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 var app = builder.Build();
 
