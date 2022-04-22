4dd1d63583ab6b06ec74463e4a21c0453dea49f5
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-04-22 09:19:46 +0200

    Made comments have an author connected to the database

--- GeoComment/Models/Comment.cs
+++ GeoComment/Models/Comment.cs
@@ -3,8 +3,8 @@
 public class Comment
 {
     public int Id { get; set; }
-    public string Author { get; set; }
     public string Message { get; set; }
     public double Longitude { get; set; }
     public double Latitude { get; set; }
+    public GeoUser Author { get; set; }
 }
