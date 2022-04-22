bde052277034d70551461d60f08985af1c3f2f99
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-04-22 09:29:55 +0200

    Added a Database class that seeds and recreates the database if it's in development.

--- GeoComment/Program.cs
+++ GeoComment/Program.cs
@@ -12,12 +12,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<GeoDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+builder.Services.AddScoped<Database>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var db = services.GetService<Database>();
+    await db.RecreateDb();
+
     app.UseSwagger();
     app.UseSwaggerUI();
 }
---
+++ GeoComment/Services/Database.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+
+namespace GeoComment.Services;
+
+public class Database
+{
+    private readonly GeoDbContext _geoDbContext;
+
+    public Database(GeoDbContext geoDbContext)
+    {
+        _geoDbContext = geoDbContext;
+    }
+
+    public async Task RecreateDb()
+    {
+        await _geoDbContext.Database.EnsureDeletedAsync();
+        await _geoDbContext.Database.EnsureCreatedAsync();
+        await SeedUsers();
+    }
+
+    public async Task SeedUsers()
+    {
+        var users = new GeoUser[]
+        {
+            new() {Name = "Ada"},
+            new() {Name = "Bill"}
+        };
+
+        await _geoDbContext.Authors.AddRangeAsync(users);
+        await _geoDbContext.SaveChangesAsync();
+    }
+}
