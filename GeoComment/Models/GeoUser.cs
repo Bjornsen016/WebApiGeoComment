9418a21614199bcdc5df655fa1ea42de6a41aa6a
Kim Björnsen Åklint <89244587+Bjornsen016@users.noreply.github.com>
2022-04-22 09:09:54 +0200

    Created a Db Context

---
+++ GeoComment/Models/GeoUser.cs
@@ -0,0 +1,8 @@
+﻿namespace GeoComment.Models;
+
+public class GeoUser
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Comment> Comments { get; set; }
+}
---
+++ GeoComment/Services/GeoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeoComment.Services;
+
+public class GeoDbContext : DbContext
+{
+    public GeoDbContext(DbContextOptions<GeoDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<GeoUser> Authors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=GeoAPIDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
